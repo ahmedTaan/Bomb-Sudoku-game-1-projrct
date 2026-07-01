c74117bff57df88a0ebbaf3ec466c0c6983ce857
Ahmed <ahmed.t4a3@gmail.com>
2026-07-01 12:54:06 +0300

    edit the file position and try to add a property in fast pen

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 let squers = document.querySelectorAll(".sqr")
 let buttons = document.querySelectorAll(".input")
 let message = document.querySelector("#message")
-
-/* ملء الشبكة بشكل مبدئي */
+let pen = document.getElementById("pen")
+/* 0_ ملء الشبكة بشكل مبدئي */
 let sqr1 = document.querySelector("#row-1-colomn-8")
 let sqr2 = document.querySelector("#row-1-colomn-9")
 let sqr3 = document.querySelector("#row-2-colomn-2")
@@ -66,19 +66,19 @@ sqr28.textContent = "4"
 sqr29.textContent = "3"
 sqr30.textContent = "9"
 sqr31.textContent = "8"
-solMatrix = [ 
-    [3,5,9,4,1,8,2,6,7],
-    [7,1,2,6,3,4,5,9,8],
-    [2,4,3,5,7,9,8,1,6],
-    [5,9,7,8,2,1,6,4,3],
-    [1,6,4,3,5,7,9,8,2],
-    [4,2,8,1,9,6,3,7,5],
-    [8,3,6,7,4,5,1,2,9],
-    [6,7,5,9,8,2,4,3,1],
-    [9,8,1,2,6,3,7,5,4]
+solMatrix = [
+    [3, 5, 9, 4, 1, 8, 2, 6, 7],
+    [7, 1, 2, 6, 3, 4, 5, 9, 8],
+    [2, 4, 3, 5, 7, 9, 8, 1, 6],
+    [5, 9, 7, 8, 2, 1, 6, 4, 3],
+    [1, 6, 4, 3, 5, 7, 9, 8, 2],
+    [4, 2, 8, 1, 9, 6, 3, 7, 5],
+    [8, 3, 6, 7, 4, 5, 1, 2, 9],
+    [6, 7, 5, 9, 8, 2, 4, 3, 1],
+    [9, 8, 1, 2, 6, 3, 7, 5, 4]
 ]
 
-let span1 = document.createElement("span")
+/* let span1 = document.createElement("span")
 span1.id = "span-1", span1.textContent = "1", span1.classList = "pencil"
 let span2 = document.createElement("span")
 span2.id = "span-2", span2.textContent = "2", span2.classList = "pencil"
@@ -96,7 +96,7 @@ let span8 = document.createElement("span")
 span8.id = "span-8", span8.textContent = "8", span8.classList = "pencil"
 let span9 = document.createElement("span")
 span9.id = "span-9", span9.textContent = "9", span9.classList = "pencil"
-
+ */
 /* squers[13].appendChild(span9.cloneNode(true))
 squers[13].appendChild(span8.cloneNode(true))
 squers[13].appendChild(span7.cloneNode(true))
@@ -121,11 +121,6 @@ squers[2].appendChild(span1.cloneNode(true))
 squers[2].removeChild(squers[2].querySelector("#span-1"))
  */
 
-
-
-
-
-
 let penMode = false
 let trackMode = false
 let lastClick = ""
@@ -137,20 +132,20 @@ let filledSquares = []
 let realPosition
 let row
 let colomn
-
+let first = true
+let pencilCurrentMatrix = []
 
 squaresForSearch.forEach(sqr => {
-    if (Boolean(sqr.textContent)){
+    if (Boolean(sqr.textContent)) {
         filledSquares.push(sqr)
     }
 })
 
-
-/* دالة المحل الحالي */
+/* 1_ دالة المحل الحالي تخليه محاط بالأزرق */
 let addPadding = (event) => {
     if (event.target.spellcheck === true) {
         if (lastClick == "") {
-            // هذا الي اعتبرته مؤشر للمكان، وهو شي غبي ما له قيمة موجود في كل عنصر.
+            // هذا الي اعتبرته مؤشر للمكان، وهو شي غبي ما له قيمة، موجود في كل عنصر.
             event.target.spellcheck = false
             lastClick = event.target.id
             event.target.style.border = "blue solid 2px"
@@ -168,50 +163,13 @@ let addPadding = (event) => {
         }
     }
     else {
+        //إزالة العلامة إذا ضغط عليها وهي مفعلة.
         event.target.spellcheck = true
         event.target.style.border = "#939598 1px solid"
     }
 }
-/* دالة فرعية، لوظائف الدوال */
-let numericFunctionFuns = (event) => {
-    if (event.target.spellcheck === true && event.target.id == "trac-pen") {
-        event.target.spellcheck = false
-        event.target.style.backgroundColor = "blue"
 
-    }
-    else if (event.target.spellcheck === true) {
-        if(event.target.id == "pen"){
-            fastPencilFunction()
-        }
-        event.target.spellcheck = false
-    }
-    else {
-        event.target.style.backgroundColor = ""
-        event.target.spellcheck = true
-    }
-}
-/* دالة فرعية لإدخال الأرقام (الكبيرة والصغيرة والمتتبعة) */
-let numericFunctionNums = (event) => {
-    current = squaresForSearch.find(sqr => sqr.id === String(lastClick))
-    if (Boolean(current)) {
-        position = current
-        position = String(position.id).split("-")
-        if (solMatrix[position[1] - 1][position[3] - 1] == event.target.textContent){
-            current.textContent = event.target.textContent
-            console.log(`${event.target.textContent} is correct answer in ${position}`)
-            filledSquares.push(current)
-            if (filledSquares.length == 81){
-                console.log(filledSquares)
-                message.textContent = "فزت!"
-            }
-        }
-        else{
-
-            console.log(`The solution is ${solMatrix[position[1] - 1][position[3] - 1]}`)
-        }
-    }
-}
-
+// 2_ دالة بايخة بس تطرش لدالة الأرقام إذا كان رقم ولدالة الدوال إذا كانت دالة
 let numericFunction = (event) => {
     if (event.target.classList[0] == "funs") {
         numericFunctionFuns(event)
@@ -219,16 +177,63 @@ let numericFunction = (event) => {
     if (event.target.classList[0] == "num") {
         numericFunctionNums(event)
     }
-
 }
 
+// 2.1_ دالة فرعية لإدخال الأرقام (الكبيرة والصغيرة) 
+let numericFunctionNums = (event) => {
+    current = squaresForSearch.find(sqr => sqr.id === String(lastClick))
+    if (Boolean(current)) {
+        position = current
+        position = String(position.id).split("-")
+        if (solMatrix[position[1] - 1][position[3] - 1] == event.target.textContent && pen.spellcheck == true
+        ) {
+            current.textContent = event.target.textContent
+            console.log(`${event.target.textContent} is correct answer in ${position}`)
+            filledSquares.push(current)
+            if (filledSquares.length == 81) {
+                message.textContent = "فزت!"
+            }
+        }
+        else if (pen.spellcheck == false) {
+            position = (Number(position[1]) - 1) * 9 + Number(position[3]) - 1
+            if (Boolean(squers[position].querySelector(String("#span-") + String(event.target.id)))) {
+                squers[position].removeChild(squers[position].querySelector(String("#span-") + event.target.id))
+            }
+            else {
+                let span = document.createElement("span")
+                span.id = String("span-" + event.target.id), span.textContent = String(event.target.id), span.classList = "pencil"
+                squers[position].appendChild(span)
+            }
+        }
+        else {
+            console.log(`The solution is ${solMatrix[position[1] - 1][position[3] - 1]}`)
+        }
+    }
+}
+// 2.2_ دالة فرعية، لوظائف الدوال 
+let numericFunctionFuns = (event) => {
+    if (event.target.id == "fast-pen") {
+        fastPencilFunction()
+    }
+    if (event.target.spellcheck === true && event.target.id == "pen") {
+        event.target.spellcheck = false
+        event.target.style.backgroundColor = "blue"
+
+    }
+    else if (event.target.spellcheck === true) {
+        event.target.spellcheck = false
+    }
+    else {
+        event.target.style.backgroundColor = ""
+        event.target.spellcheck = true
+    }
+}
+
+
+// a_ الدالة الي تتحقق من الشروط وتسمح له بإدخال رقم.
 let rowCodition = true
 let colomnCondition = true
 let adjacentCondition = true
-
-let rowColomnMatrix = String("row-6-colomn-9").split("-")
-
-
 let checker = (num, position) => {
 
     rowCodition = true
@@ -236,7 +241,7 @@ let checker = (num, position) => {
     adjacentCondition = true
 
     let rowColomnMatrix = String(position).split("-")
-    
+
     //شرط الصفوف
     for (let i = 1; i <= 9; i++) {
         rowColomnMatrix[1] = i
@@ -246,11 +251,11 @@ let checker = (num, position) => {
             )
         ).textContent) {
             rowCodition = false
-            console.log(`I find the row mach at ${rowColomnMatrix.join("-")}`)
+            // console.log(`I find the row mach at ${rowColomnMatrix.join("-")}`)
         }
     }
     rowColomnMatrix = String(position).split("-")
-    
+
     //شرط الأعمدة
     for (let i = 1; i <= 9; i++) {
         rowColomnMatrix[3] = i
@@ -260,7 +265,7 @@ let checker = (num, position) => {
             )
         ).textContent) {
             colomnCondition = false
-            console.log(`I find the colomn mach at ${rowColomnMatrix.join("-")}`)
+            // console.log(`I find the colomn mach at ${rowColomnMatrix.join("-")}`)
         }
     }
     //شرط الجوار
@@ -284,53 +289,77 @@ let checker = (num, position) => {
                 )
             ).textContent) {
                 adjacentCondition = false
-                console.log(`I find the adjacent maching at ${rowColomnMatrix.join("-")}`)
+                // console.log(`I find the adjacent maching at ${rowColomnMatrix.join("-")}`)
             }
         }
     }
     return (rowCodition && colomnCondition && adjacentCondition)
 }
 
-//الدالة الي تستقبل موقع وتحط فيه الأرقام الممكنة حسب الشروط
+// b_ الدالة الي تستقبل موقع وتحط فيه الأرقام الممكنة حسب الشروط
 let pencilFunction = (position) => {
     // تحول المكان من الصف والعمود إلى رقم من 0 إلى 80
     realPosition = position.split("-")
     realPosition = (Number(realPosition[1]) - 1) * 9 + Number(realPosition[3]) - 1
     //يحذف أي بنسل موجود
-    for (let n=1; n<=9; n++){
-        if (Boolean(squers[realPosition].querySelector(String("#span-") + n))){
-        squers[realPosition].removeChild(squers[realPosition].querySelector(String("#span-") + n))
+    for (let n = 1; n <= 9; n++) {
+        if (Boolean(squers[realPosition].querySelector(String("#span-") + n))) {
+            squers[realPosition].removeChild(squers[realPosition].querySelector(String("#span-") + n))
         }
     }
     //يحط البنسل للأرقام الممكنة (حسب الشروط الثلاثة)
-    if(!Boolean(squers[realPosition].textContent)){
-        for (let n=1; n<=9; n++){
-        if(checker(n,position)){
-            let span = document.createElement("span")
-            span.id = String("span-" + n), span.textContent = String(n), span.classList = "pencil"
+    if (!Boolean(squers[realPosition].textContent)) {
+        for (let n = 1; n <= 9; n++) {
+            if (checker(n, position)) {
+                let span = document.createElement("span")
+                span.id = String("span-" + n), span.textContent = String(n), span.classList = "pencil"
 
-            squers[realPosition].appendChild(span.cloneNode(true))
+                squers[realPosition].appendChild(span.cloneNode(true))
+            }
         }
     }
 }
-}
 
+// 3_ تستدعيها عشان تملأ الأماكن الباقية بالاحتمالات الممكنة
 let fastPencilFunction = () => {
-    for (let n=0; n<=80; n++){
-        //تحول من رقم إلى موقع
-        row = Math.floor(n/9) + 1
-        colomn = Math.round((n/9 - Math.floor(n/9))*9) + 1
-        position = String("row-" + row + "-colomn-" + colomn)
-        pencilFunction(position)
+    // يحط كل الأرقام الممكنة (في أول مرة فقط)
+    /* if (first) {
+        first = false */
+        for (let n = 0; n <= 80; n++) {
+            //تحول من رقم إلى موقع
+            row = Math.floor(n / 9) + 1
+            colomn = Math.round((n / 9 - Math.floor(n / 9)) * 9) + 1
+            position = String("row-" + row + "-colomn-" + colomn)
+            //تستدعي الدالة الي تحط في هذا الموقع
+            pencilFunction(position)
+        }
+/*     }
+ */    // يحط كل الأرقام الممكنة باستثناء الأرقام الي هو شالها من قبل
+/*     else {
+        for (let n = 0; n <= 80; n++) {
+            //تحول من رقم إلى موقع
+            row = Math.floor(n / 9) + 1
+            colomn = Math.round((n / 9 - Math.floor(n / 9)) * 9) + 1
+            position = String("row-" + row + "-colomn-" + colomn)
+            realPosition = ((row - 1) * 9) + (colomn - 1)
+            pencilCurrentMatrix = []
+            for (let n = 1; n <= 9; n++) {
+                if (Boolean(squers[realPosition].querySelector(String("#span-") + n))){
+                pencilCurrentMatrix.push(squers[realPosition].querySelector(String("#span-") + n))
+                }
+            }
+            //تستدعي الدالة الي تحط في هذا الموقع
+            pencilFunction(position)
+            //تحذف الي ما كان موجود
+            console.log()
+            for (let n = 1; n <= 9; n++){
+                if(Boolean(squers[realPosition].querySelector(String("#span-") + n)) && pencilCurrentMatrix.indexOf( squers[realPosition].querySelector(String("#span-") + n) ) == -1){
+                    squers[realPosition].removeChild(squers[realPosition].querySelector(String("#span-") + n))
+                }
+            }
+        }
     }
-}
+ */}
 
-console.log(checker(1, "row-9-colomn-9"))
-
-console.log(String(String("position").slice(0, 6) + String(5)))
-
-console.log(squers)
-console.log(buttons)
-console.log(sqr1)
 squers.forEach(element => element.addEventListener("click", addPadding))
 buttons.forEach(element => element.addEventListener("click", numericFunction))
